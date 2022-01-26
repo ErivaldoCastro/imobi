1d679de6ad53826dafef1bc75f47978a2d9cc35a
Erivaldo Castro <erivaldodocastro@gmail.com>
2022-01-26 15:13:38 -0300

    first commit

---
+++ autenticacao/urls.py
@@ -0,0 +1,13 @@
+from django import urls
+from django.urls import path
+from django.urls import path
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('logar/', views.logar, name='logar'),
+    path('sair/', views.sair, name='sair')
+]
---
+++ plataforma/admin.py
@@ -0,0 +1,24 @@
+from email.mime import image
+import imp
+from django.contrib import admin
+from .models import Cidade, DiasVisita, Horario, Imovei, Imagem, Visitas
+
+@admin.register(Imovei)
+class ImoveiAdmin(admin.ModelAdmin):
+    list_display = ('rua', 'valor', 'quartos', 'tamanho', 'cidade', 'tipo')
+    list_editable = ('valor', 'tipo')
+    list_filter = ('cidade', 'tipo')
+
+admin.site.register(DiasVisita)
+admin.site.register(Cidade)
+admin.site.register(Horario)
+admin.site.register(Imagem)
+admin.site.register(Visitas)
+
+
+
+
+
+
+
+
